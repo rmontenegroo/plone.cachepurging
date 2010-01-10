1b36403bf491d0522f1bdfe2efa92199753b00e3
Martin Aspeli <optilude@gmail.com>
2010-01-10 10:11:40 +0000

    Add tests for the rewriter
    svn path=/plone.cachepurging/trunk/; revision=32927

--- plone/cachepurging/rewrite.py
+++ plone/cachepurging/rewrite.py
@@ -1,7 +1,7 @@
 import urlparse
 
 from zope.interface import Interface, implements
-from zope.component import adapts, getUtility
+from zope.component import adapts, queryUtility
 
 from plone.registry.interfaces import IRegistry
 
@@ -26,7 +26,10 @@ class DefaultRewriter(object):
         if virtualURL is None:
             return [path]
         
-        registry = getUtility(IRegistry)
+        registry = queryUtility(IRegistry)
+        if registry is None:
+            return [path]
+        
         settings = registry.forInterface(ICachePurgingSettings, check=False)
         
         virtualHosting = settings.virtualHosting
@@ -35,18 +38,23 @@ class DefaultRewriter(object):
         if not virtualHosting:
             return [path]
         
-        domains = settings.domains
-        if not domains:
-            domains = [request.get('SERVER_URL')]
-        
         # We need to reconstruct VHM URLs for each of the domains
         virtualUrlParts = request.get('VIRTUAL_URL_PARTS')
         virtualRootPhysicalPath = request.get('VirtualRootPhysicalPath')
 
         # Make sure request is compliant
-        if not virtualUrlParts or not virtualRootPhysicalPath \
-            or len(virtualUrlParts) < 2 or len(virtualUrlParts) > 3:
+        if (not virtualUrlParts
+         or not virtualRootPhysicalPath
+         or not isinstance(virtualUrlParts, (list,tuple,))
+         or not isinstance(virtualRootPhysicalPath, (list,tuple,))
+         or len(virtualUrlParts) < 2
+         or len(virtualUrlParts) > 3
+        ):
             return [path]
+        
+        domains = settings.domains
+        if not domains:
+            domains = [virtualUrlParts[0]]
         
         # Virtual root, e.g. /Plone. Clear if we don't have any virtual root
         virtualRoot = '/'.join(virtualRootPhysicalPath)
