0c8c99cdb75ee4613c4dfdbde2a8fcd00c284896
Maurits van Rees <maurits@vanrees.org>
2019-10-09 22:26:14 +0200

    Added development status 5 production/stable to classifiers.
    [ci skip]

--- setup.py
+++ setup.py
@@ -10,6 +10,7 @@ setup(
         open("README.rst").read() + "\n" + open("CHANGES.rst").read()
     ),
     classifiers=[
+        "Development Status :: 5 - Production/Stable",
         "Framework :: Plone",
         "Framework :: Plone :: 5.2",
         "License :: OSI Approved :: GNU General Public License v2 (GPLv2)",
